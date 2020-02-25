39725b7da12ca2c66ad444a56bf16c1c4aec93b6
Minh <59575918+mngo3@users.noreply.github.com>
2020-02-25 11:40:42 -0700

    Update D - Simple Joins Exercise.sql

--- SQL/D - Simple Joins Exercise.sql
+++ SQL/D - Simple Joins Exercise.sql
@@ -41,15 +41,15 @@ ORDER BY 'Staff Full Name', CourseId
 --3.	Select all the Club ID's and the Student full names that are in them
 -- TODO: Student Answer Here...
 SELECT  ClubId, FirstName + ' ' + LastName AS 'Student Full Name'
-FROM    Activity A
+FROM    Activity AS A
     INNER JOIN Student S ON A.StudentID = S.StudentID
 
 --4.	Select the Student full name, courseID's and marks for studentID 199899200.
 SELECT  S.FirstName + ' ' + S.LastName AS 'Student Name',
         R.CourseId,
         R.Mark
-FROM    Registration R
-    INNER JOIN Student S
+FROM    Registration AS R
+    INNER JOIN Student AS S
             ON S.StudentID = R.StudentID
 WHERE   S.StudentID = 199899200
 
@@ -84,15 +84,28 @@ SELECT  FirstName + ' ' + LastName AS 'Student',
         CourseName
 --        , S.StudentID -- Used for WHERE clause
 --        , R.Semester  -- Used for WHERE clause
-FROM    Student S
-    INNER JOIN Registration R ON S.StudentID = R.StudentID
+FROM    Student AS S
+    INNER JOIN Registration AS R ON S.StudentID = R.StudentID
     INNER JOIN Course C       ON R.CourseId = C.CourseId
 WHERE   S.StudentID = 199912010
   AND   R.Semester = '2001S'
 
 --9. What are the Student Names, courseID's with individual Marks at 80% or higher? Sort the results by course.
 -- TODO: Student Answer Here...
-
+SELECT  FirstName + '' + LastName AS 'Student Names',
+        R.CourseId, Mark
+FROM    Student AS S
+    INNER JOIN Registration AS R 
+                    ON S.StudentID = R.StudentID
+                    ORDER BY R.CourseId
 --10. Modify the script from the previous question to show the Course Name along with the ID.
 -- TODO: Student Answer Here...
-
+SELECT  FirstName + '' + LastName AS 'Student Names',
+        R.CourseId, Mark,
+        C.CourseName
+FROM    Student AS S
+    INNER JOIN Registration AS R 
+         ON S.StudentID = R.StudentID
+    INNER JOIN Course AS C
+         ON R.CourseId = C.CourseId          
+                    ORDER BY R.CourseId
