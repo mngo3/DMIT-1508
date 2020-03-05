3c3fc3a501ca57c59f971df7d75fb8862756dd1c
Minh <59575918+mngo3@users.noreply.github.com>
2020-03-05 12:37:25 -0700

    a

--- SPROCS/A - Stored Procedures.sql
+++ SPROCS/A - Stored Procedures.sql
@@ -83,7 +83,8 @@ AS
     HAVING AVG(R.Mark) > 80
 RETURN
 GO
-
+EXEC HonorCoursesOneTerm
+GO
 --3.B. Your instructor is back, and recommends that the previous stored procedure use a parameter for the semester, making it more "re-usable"
 ALTER PROCEDURE HonorCoursesOneTerm
     @Semester   char(5) -- @ preceeds the name of the parameter
@@ -167,12 +168,31 @@ GO
 EXEC LowNumbers
 INSERT INTO Course(CourseId, CourseName, CourseHours, CourseCost, MaxStudents)
 VALUES ('DMIT987', 'Advanced Logic', 90, 420.00, 12)
-
+GO
 --6. Create a stored procedure called "Provinces" to list all the students provinces.
 
+CREATE PROCEDURE Provinces
+AS
+	SELECT	Province
+	FROM	Student
+RETURN
+GO
+EXEC Provinces
+GO
 --7. OK, question 6 was ridiculously simple and serves no purpose. Lets remove that stored procedure from the database.
-
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = N'PROCEDURE' AND ROUTINE_NAME = 'Provinces')
+    DROP PROCEDURE Provinces
+GO
 --8. Create a stored procedure called StudentPaymentTypes that lists all the student names and their payment types. Ensure all the student names are listed, including those who have not yet made a payment.
-
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = N'PROCEDURE' AND ROUTINE_NAME = 'StudentPaymentTypes')
+    DROP PROCEDURE StudentPaymentTypes
+GO
+CREATE PROCEDURE StudentPaymentTypes
+AS	
+	SELECT  S.FirstName,LastName,
+			P.PaymentID
+	FROM	Student AS S 
+		INNER JOIN Payment AS P ON S.StudentID = P.StudentID
+	
 --9. Modify the procedure from question 8 to return only the student names that have made payments.
 
